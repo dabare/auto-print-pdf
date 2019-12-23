b20efe9772b2d334b2d88781c9fca57ef6d8c859
Madhava_09466 <madhava.dabare@dialog.lk>
2019-12-24 01:08:58 +0530

    added soffice

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("phelper.exe", "-print-to", os.Getenv("POS_PRINTER") , os.Args[1])
+	cmd := exec.Command("soffice.exe", "--pt", os.Getenv("POS_PRINTER"), os.Args[1])
 	_, err := cmd.Output()
 	if err != nil {
 		if err != nil {
